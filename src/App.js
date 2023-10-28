6a6ba7187fb5b3e4ab8503fa07f6a89a1284aaeb
sophiasundar <sophiagifta17@gmail.com>
2023-10-28 16:00:28 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
-import CardItem from './Components/Carditem';
+import React, { useState } from "react";
 
 
 
-function App() {
+export default function App() {
+
+  const [value, setValue] = useState(0);
+  
   return (
     <div className="App">
-      
-
-
+      <h4>{`counter value: ${value}`}</h4>
+     
+     <Counter></Counter>
+     <button onClick = { ()=>setValue(value+1)
+  }
+    
+    >Counter</button>
+     
 
     </div>
   );
 }
 
-export default App; 
+
+
+
+const Counter = () => {
+
+  
+   
+
+  }
